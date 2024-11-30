74ff2db366897de39c2cc08847ca0a451a6687f6
gfabreu <gabrielabreu.sp@gmail.com>
2024-11-30 03:05:28 +0000

    modificando a frase 2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 function Home() {
-    return <h1>Camila, eu amo você. Se você me ama, dê um sorrisinho! ❤️</h1>
+    return <h1>Camila, meu amor, obrigado por todo o apoio. Sem você, eu não sei como seria minha vida.
+Se você me ama, deixe escapar um sorrisinho, porque o meu coração sorri sempre que estou ao seu lado! 
+Eu te amo muito! ❤️</h1>
 }
 export default Home;
