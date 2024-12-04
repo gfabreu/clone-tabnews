d649bf385e7b5b64b0ac5d306a59bfa83a37300f
gfabreu <gabrielabreu.sp@gmail.com>
2024-12-04 17:48:43 +0000

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 function Home() {
-    return <h1>Camila, meu amor, obrigado por todo o apoio. Sem você, eu não sei como seria minha vida.
-Se você me ama, deixe escapar um sorrisinho, porque o meu coração sorri sempre que estou ao seu lado! 
-Eu te amo muito! ❤️</h1>
+  return (
+    <h1>
+      Camila, meu amor, obrigado por todo o apoio. Sem você, eu não sei como
+      seria minha vida. Se você me ama, deixe escapar um sorrisinho, porque o
+      meu coração sorri sempre que estou ao seu lado! Eu te amo muito! ❤️
+    </h1>
+  );
 }
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("identação errada");
+}
+
 export default Home;
